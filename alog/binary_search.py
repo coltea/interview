b44409d8602da4ebd33310c3859d20701c0246a0
coltea <751848100@qq.com>
2019-08-23 14:50:36 +0800

    fibonacci

--- alog/binary_search.py
+++ alog/binary_search.py
@@ -1,15 +1,20 @@
-def binary_search(items: list, item: int):
-    print(items, item)
-    if item > items[-1] or item < items[0] or len(items) == 0:
+"""
+二分法
+"""
+
+
+def binary_search(lt, target):
+    print(f"lt: {lt}, target:{target}")
+    if target < lt[0] or target > lt[-1] or len(lt) == 0:
         return False
-    n = len(items) // 2
-    if items[n] == item:
+    mid = len(lt) // 2
+    if lt[mid] == target:
         return True
+    elif lt[mid] > target:
+        return binary_search(lt[:mid], target)
     else:
-        if items[n] < item:
-            return binary_search(items[n:], item)
-        else:
-            return binary_search(items[:n], item)
+        return binary_search(lt[mid:], target)
 
 
-print(binary_search([1, 2, 3, 4, 5], 4))
+if __name__ == '__main__':
+    print(binary_search(list(range(1, 15)), 13))
